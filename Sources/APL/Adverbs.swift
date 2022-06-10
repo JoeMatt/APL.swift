7b4dc1383fe05c2d5855601826d754fa11c16dd9
Joseph Mattello <mail@joemattiello.com>
2022-06-10 04:49:38 -0400

    use typealiases for some things
    Signed-off-by: Joseph Mattello <mail@joemattiello.com>

--- Sources/APL/Adverbs.swift
+++ Sources/APL/Adverbs.swift
@@ -11,30 +11,33 @@ import Foundation
 infix operator /
 infix operator ⧵
 
-public func /<A>(f: A -> A -> A, xs0: [A]) -> A {
+public typealias DoubleFunc<A> = A -> A
+public typealias TripleFunc<A> = A -> A -> A
+
+public func /<A>(f: TripleFunc<A>, xs0: [A]) -> A {
 	let hd = xs0[0]
 	let tl = Array<A>(xs0[1..<xs0.count])
 	return foldl(f)(initial: hd)(xss: tl)
 }
 
-public func ⧵<A>(f: A -> A -> A, xs0: [A]) -> [A] {
+public func ⧵<A>(f: TripleFunc<A>, xs0: [A]) -> [A] {
 	return f ⧵ xs0.map({ (let x) in
 		return [x]
 	})
 }
 
-public func ⧵<A>(f: A -> A -> A, xs0: [[A]]) -> [A] {
-	var arr = ⍉xs0
-	return arr.map({ (let xs) in
+public func ⧵<A>(f: TripleFunc<A>, xs0: [[A]]) -> [A] {
+	let arr = ⍉xs0
+	return arr.map({ (xs) in
 		let hd = xs[0]
 		let tl = xs[1..<xs.count]
-		return foldl(f)(initial: hd)(xss: Array<A>(tl))
+		return foldl(f, initial: hd, xss: Array<A>(tl))
 	})
 }
 
 infix operator •
 
-public func •<A>(var f : A -> A, times: UInt) -> A -> A {
+public func •<A>(var f : DoubleFunc<A>, times: UInt) -> DoubleFunc<A> {
 	return { (let x) in
 		var acc = x
 		for _ in 0...times {
@@ -62,7 +65,8 @@ public func ¨<T, U>(_ x : T, _ f : T -> U -> T) -> U -> T {
     return f(x)
 }
 
-public func foldl<A, B>(_ f: B -> A -> B)(initial: B)(xss: [A]) -> B {
+public typealias FodlFunc<A, B> = B -> A -> B
+public func foldl<A, B>(_ f: FodlFunc<A,B>, initial: B, xss: [A]) -> B {
 	var xs = initial
 	for x in xss {
 		xs = f(xs)(x)
